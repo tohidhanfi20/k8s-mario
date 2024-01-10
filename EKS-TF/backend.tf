26d832e5ba9c3e7412e675ff97541f1f303178fa
Tohid Hanfi <119034342+tohidhanfi20@users.noreply.github.com>
2024-01-10 18:57:13 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario-game-by-prajwal" # Replace with your actual S3 bucket name
+    bucket = "mario-game-by-Tohid" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
